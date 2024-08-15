acbee82033bab0bf74bcf5aa0573cf4f260986fc
kuguardt <kuguardt@gmail.com>
2024-08-14 17:01:59 -0700

    basic2

--- Assets/Script/PlayerNetwork.cs
+++ Assets/Script/PlayerNetwork.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
-
     private void Update()
     {
-        Vector3 moveDIr = new Vector3(0,0,0);
+        if (!IsOwner) { return; }
+
+        Vector3 moveDIr = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W)) moveDIr.z = +1f;
         if (Input.GetKey(KeyCode.S)) moveDIr.z = -1f;
